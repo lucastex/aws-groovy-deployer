91802bd7fd9333fff1279dddf9e766be0c7e818c
lucastex <lucastex@gmail.com>
2011-05-15 01:47:23 -0300

    better output on checking application status

--- src/DeployHelper.groovy
+++ src/DeployHelper.groovy
@@ -12,20 +12,23 @@ class DeployHelper {
 			
 			instances.each { instance -> 
 			
-				try {
-					def urlToCheck = "http://${instance.publicDnsName}:8080"
-					ConsoleUtil.debug "Checking application in instance [${instance.instanceId}] - ${urlToCheck}/index.jsp"
+				if (instanceIds.contains(instance.instanceId)) {
+					
+					try {
+						def urlToCheck = "http://${instance.publicDnsName}:8080"
+						ConsoleUtil.debug "Checking application in instance [${instance.instanceId}] - ${urlToCheck}/index.jsp"
 			
-					def http = new HTTPBuilder(urlToCheck)
-					http.get( path : '/index.jsp', contentType : "text/plain") { resp, reader ->
-						if (resp.status == 200) {
-							ConsoleUtil.debug "\tStatus: [OK]"
-							instanceIds.remove(instance.instanceId)
+						def http = new HTTPBuilder(urlToCheck)
+						http.get( path : '/index.jsp', contentType : "text/plain") { resp, reader ->
+							if (resp.status == 200) {
+								ConsoleUtil.debug "\tStatus: [OK]"
+								instanceIds.remove(instance.instanceId)
+							}
 						}
-					}
-				} catch (Exception e) {
-					ConsoleUtil.debug "\tStatus: [NOT OK]"
-				}		
+					} catch (Exception e) {
+						//ConsoleUtil.debug "\tStatus: [NOT OK]"
+					}		
+				}
 			}
 		}
 	}
